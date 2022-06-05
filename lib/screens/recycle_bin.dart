eddd27ab88aeeabc808a77b3f3d777df4f5ef9e1
banku27 <pankajparihar123321@gmail.com>
2022-06-05 22:24:06 +0530

    recycle bin

---
+++ lib/screens/recycle_bin.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/Bloc/bloc/tasks_bloc.dart';
+import 'package:todo_app/models/task.dart';
+import 'package:todo_app/screens/add_task_screen.dart';
+import 'package:todo_app/screens/my_drawer.dart';
+
+import 'package:todo_app/widget/tasks_list.dart';
+
+class RecycleBin extends StatelessWidget {
+  const RecycleBin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recycle Bin'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      drawer: const MyDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Center(
+            child: Chip(
+              label: Text(
+                'Tasks',
+              ),
+            ),
+          ),
+          TasksList(taskList: [])
+        ],
+      ),
+    );
+  }
+}
